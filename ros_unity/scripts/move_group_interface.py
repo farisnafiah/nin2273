a6b96908f7d9489ec0644b986d4fcd26aa2e4f51
Faris Nafiah <nafiahfarisizzuddin@gmail.com>
2023-10-18 20:05:08 +0100

    Workpiece service

--- ros_unity/scripts/move_group_interface.py
+++ ros_unity/scripts/move_group_interface.py
@@ -10,6 +10,7 @@ import std_srvs.srv
 from moveit_commander.conversions import pose_to_list
 from ros_unity.srv import RobotMoverService, RobotMoverServiceRequest, RobotMoverServiceResponse
 from ros_unity.srv import JointGoal, JointGoalRequest, JointGoalResponse
+from ros_unity.srv import CylindricalWorkpiece, CylindricalWorkpieceRequest, CylindricalWorkpieceResponse
 
 
 class MoveGroupInterface:
@@ -21,18 +22,30 @@ class MoveGroupInterface:
         self.scene = moveit_commander.PlanningSceneInterface()
         
         self.group_name = "manipulator"
-        self.response = RobotMoverServiceResponse()
+        
         self.plan_execute_path_subscriber = rospy.Service('/moveit', RobotMoverService, self.PlanExecutePath)
         self.joint_goal_service = rospy.Service('/joint_goal', JointGoal, self.GoToJointGoal)
+        self.workpiece_origin_service = rospy.Service('/cylindrical_workpiece_parameters', CylindricalWorkpiece, self.CreateCylindricalObstacle)
 
 
         self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
-        print("Ready to plan")
-        #self.move_group_goal_publisher = rospy.Publisher('move_group/goal', moveit_msgs.msg.)
+        print("Unity services are running...")
         self.display_trajectory_publisher = rospy.Publisher('move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size = 1)
         self.plan = None
         self.fraction = None
         rospy.spin()
+
+
+
+
+
+
+
+
+
+
+
+
 
     # Plans or executes the path (or both) depending on the button pressed in Unity (Plan, Execute, Plan and Execute)
     def PlanExecutePath(self, req):
@@ -72,6 +85,15 @@ class MoveGroupInterface:
         return self.response
         
         
+
+
+
+
+
+
+
+
+
         
         
         
@@ -101,6 +123,76 @@ class MoveGroupInterface:
         
         print(self.move_group.get_current_joint_values())
         return response
+    
+
+
+
+
+
+
+
+
+
+    def CreateCylindricalObstacle(self, req):
+        print("Creating cylindrical obstacle...")
+        
+        
+        print("Request: \n{}".format(req))
+
+        cylinder_pose = geometry_msgs.msg.PoseStamped()
+        cylinder_pose.header.frame_id = self.move_group.get_planning_frame()
+        cylinder_pose.pose = req.origin
+
+        self.scene.add_cylinder("workpiece", cylinder_pose, req.height.data, req.radius.data)
+
+        response = CylindricalWorkpieceResponse()
+        response.status.data = True
+
+        return response
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def wait_for_state_update(
+        self, box_is_known=False, timeout=4, object_name="workpiece"):
+        
+        start = rospy.get_time()
+        seconds = rospy.get_time()
+        while (seconds - start < timeout) and not rospy.is_shutdown():
+            # Test if the box is in attached objects
+            attached_objects = self.scene.get_attached_objects([object_name])
+            is_attached = len(attached_objects.keys()) > 0
+
+            # Test if we are in the expected state
+            if (object_name in self.scene.get_known_object_names()):
+                return True
+
+            # Sleep so that we give other threads time on the processor
+            rospy.sleep(0.1)
+            seconds = rospy.get_time()
+
+        # If we exited the while loop without returning then we timed out
+        return False
+
+
+
+
+
+
+
+
+
+
 
 
 
