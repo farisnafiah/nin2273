12db42a96da2808eef734996d599550343bfb355
farisnafiah <nafiahfarisizzuddin@gmail.com>
2023-12-01 12:19:39 +0000

    Package for simulated Sonus Evo

--- SONUS_EVO_SIMULATION/package_for_comms/scripts/AscanTalker.py
+++ SONUS_EVO_SIMULATION/package_for_comms/scripts/AscanTalker.py
@@ -2,13 +2,14 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import Float64
+import random
 
 def AscanTalker():
     pub = rospy.Publisher('Thickness1', Float64, queue_size=10)
     rospy.init_node('AscanTalker', anonymous=True)
     rate = rospy.Rate(1) # 10hz
     while not rospy.is_shutdown():
-        fake_thickness = 9.8
+        fake_thickness = 15.5 + round(random.uniform(-0.1,0.1), 2)
         pub.publish(fake_thickness)
         rate.sleep()
 
